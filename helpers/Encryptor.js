bafc2077a96816f3902d05be98f88a3daa3722ef
airyng <airyng@yandex.ru>
2022-10-29 19:31:44 +0300

    feat(encryptor): added one more encryption type

--- helpers/Encryptor.js
+++ helpers/Encryptor.js
@@ -1,9 +1,18 @@
 const bcrypt = require('bcryptjs')
+const crypto = require('crypto')
 
 class Encryptor {
 
-  static hash (commonString) {
-    return bcrypt.hashSync(commonString, 10)
+  static hash (commonString, type = 'bcrypt') {
+    if (type === 'bcrypt') {
+      return bcrypt.hashSync(commonString, 10)
+    }
+    if (type === 'md5') {
+      return crypto.createHash('md5').update(commonString).digest('hex')
+    }
+    else {
+      throw new Error('Given unknown hash type.')
+    }
   }
 
   static verify (commonString, hashString) {
