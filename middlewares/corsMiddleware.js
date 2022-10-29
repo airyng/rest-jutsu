06e600b6d531ecca5e73bd1dd5fc411d5479b49a
airyng <airyng@yandex.ru>
2022-10-29 19:30:21 +0300

    fix(cors-middlware): fixed behavior with unset allow_origin variable

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,8 +1,8 @@
 function corsMiddleware (req, res, next) {
 
-  const availableOrigins = process.env.ALLOW_ORIGIN.split(',')
+  const availableOrigins = process.env.ALLOW_ORIGIN?.split(',')
 
-  res.append('Access-Control-Allow-Origin', availableOrigins)
+  res.append('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN ? availableOrigins : '*')
   res.append('Access-Control-Allow-Methods', '*')
   res.append('Access-Control-Allow-Headers', '*')
   next()
