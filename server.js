2fd4c757d21c2311c8ba2922100ce954fcd347ab
airyng <airyng@yandex.ru>
2022-10-29 19:22:25 +0300

    feat(server): added validation of ability to connect to db

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const express = require('express'),
       corsMiddleware = require('./middlewares/corsMiddleware'),
       port = process.env.PORT || 3000
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+if (process.env.DATABASE_URL) {
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+  const db = mongoose.connection
+  db.on('error', (error) => console.error(error))
+  db.once('open', () => console.log('Connected to Database'))
+} else 
+  console.log('[WARNING] Can\'t connect to Database without DATABASE_URL env-variable')
+      
 
 app.use(express.json())
 
