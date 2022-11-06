06929bc46a68c182f84368a51b8637c9daf10cf1
airyng <airyng@yandex.ru>
2022-11-06 13:45:04 +0300

    fix(cors-middleware): fixed issue with multiple allowed origins

--- middlewares/corsMiddleware.js
+++ middlewares/corsMiddleware.js
@@ -1,11 +1,16 @@
-function corsMiddleware (req, res, next) {
+module.exports = function (req, res, next) {
 
-  const availableOrigins = process.env.ALLOW_ORIGIN?.split(',')
+  if (!process.env.ALLOW_ORIGIN) {
+    res.append('Access-Control-Allow-Origin', '*')
+  }
+  else {
+    const availableOrigins = process.env.ALLOW_ORIGIN?.split(',')
 
-  res.append('Access-Control-Allow-Origin', process.env.ALLOW_ORIGIN ? availableOrigins : '*')
+    if (availableOrigins.includes(req.headers?.origin)) {
+      res.append('Access-Control-Allow-Origin', [req.headers?.origin])
+    }
+  }
   res.append('Access-Control-Allow-Methods', '*')
   res.append('Access-Control-Allow-Headers', '*')
   next()
 }
-
-module.exports = corsMiddleware
